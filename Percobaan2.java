33ab6e4e9f16c51c567aefb0baff2bf12f428c22
rochiszigmakinara <rochiszigmakinara@gmail.com>
2025-12-04 08:58:35 +0700

    modifikasi nomor 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -3,8 +3,10 @@ public class Percobaan2 {
 
     static int hitungPangkat(int x, int y) {
         if (y==0) {
+            System.out.print("1");
             return (1);
         } else {
+            System.out.print(x+"x");
             return (x*hitungPangkat(x, y-1));
         }
     }
@@ -17,6 +19,10 @@ public class Percobaan2 {
         bilangan= sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat= sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+
+        int hasil= hitungPangkat(bilangan, pangkat);
+        System.out.print("= "+hasil);
     }
 }
+
+
